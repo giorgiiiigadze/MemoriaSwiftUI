61168e2c016d42f02194a0ad24e57ec42527a602
Giorgi <giobuisness97@gmail.com>
2026-07-01 15:20:33 +0400

    Initial Commit

---
+++ MemoriaSwiftUI/MemoriaSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemoriaSwiftUIApp.swift
+//  MemoriaSwiftUI
+//
+//  Created by giorgi giorgadze on 01/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct MemoriaSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
